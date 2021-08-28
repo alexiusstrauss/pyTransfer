89b47f12b5993e9ce3342912aa8a80d076c6ac5e
Sourcery AI <>
2021-08-28 15:49:31 +0000

    'Refactored by Sourcery'

--- pytransfer/funcoes.py
+++ pytransfer/funcoes.py
@@ -6,21 +6,16 @@ from random import choice, getrandbits
 
 
 def genBalanceRandom():
-    #Funcao para gerar Saldo randomico - possivel de implementações e regras.
-    result = getrandbits(16)
-    return result
+    return getrandbits(16)
 
 
 def randomchar(tamanho):
     # funcao para dandomizar um char de tamanho definido
     caracteres  = '123456-78ABCDEFGHJK-MNPQRSTXabcdefghjkmnpqrstxz-'
-    result  = ''    
-    for char in range(tamanho):
-        result += choice(caracteres)
-    return result
+    return ''.join(choice(caracteres) for _ in range(tamanho))
 
 def getnewtoken():
-    # funcao para criar token no padrao sha1 usando 2 valores randomicos
-    result = hashlib.sha1((randomchar(5) + randomchar(5)).encode('utf-8')).hexdigest()
-    return result
+    return hashlib.sha1(
+        (randomchar(5) + randomchar(5)).encode('utf-8')
+    ).hexdigest()
 
