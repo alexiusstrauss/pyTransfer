f0b00683a77ba7e2d8738aa1c8db18896fddebfd
Alexius <alexius.marques@gmail.com>
2021-08-29 18:26:42 -0300

    add workflow

--- dessafio/settings.py
+++ dessafio/settings.py
@@ -106,12 +106,8 @@ DATABASES = {
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'postgres',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'pytransferDB',
         }
     }
 
