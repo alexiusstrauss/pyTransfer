7806c56a618cbc9ffe11824bd8f1c94e795e1a83
Alexius <alexius.marques@gmail.com>
2021-08-29 19:33:32 -0300

    add on CI flake8

--- pytransfer/models.py
+++ pytransfer/models.py
@@ -1,8 +1,7 @@
 import uuid
-from decimal                    import Decimal
-from django.utils.translation   import ugettext_lazy as _
-from django.db                  import models
-from django.db.models           import Sum, Q
+from decimal import Decimal
+from django.db import models
+from django.db.models import Sum
 
 
 class PessoaManager(models.Manager):
@@ -10,35 +9,34 @@ class PessoaManager(models.Manager):
     # retorna apenas as pessoas ativas
     def get_queryset(self):
         return super().get_queryset().filter(ativo=True)
-       
+
 
 class Pessoa(models.Model):
-    nome            = models.CharField(max_length=50)
-    sobrenome       = models.CharField(max_length=50)
-    nomecompleto    = models.CharField(max_length=110)
-    email           = models.EmailField()
-    telefone        = models.CharField(max_length=20)
-    cpf             = models.CharField(max_length=20)
-    ativo           = models.BooleanField(default=True)
-    created_at      = models.DateTimeField(auto_now_add=True)
-    objects         = PessoaManager()   # Manager de Pessoas
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    nomecompleto = models.CharField(max_length=110)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    cpf = models.CharField(max_length=20)
+    ativo = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = PessoaManager()   # Manager de Pessoas
 
     class Meta:
         verbose_name = ('Pessoa')
         verbose_name_plural = ('Pessoas')
 
     def __str__(self):
-        return  '%s | Saldo:  R$ %s' % (self.nomecompleto, self.balance)
-
+        return '%s | Saldo:  R$ %s' % (self.nomecompleto, self.balance)
 
     @property
-    def token(self):       
+    def token(self):
         result = Token.objects.get(Pessoa=self)
         return result.codigo
 
     @property
-    def balance(self): 
-        result = Decimal('0.00000')      
+    def balance(self):
+        result = Decimal('0.00000')
 
         # Soma valores bloqueados em transações
         bloqueado = History.objects.filter(remetente=self).filter(
@@ -58,19 +56,17 @@ class Pessoa(models.Model):
         result = carteira - bloqueado
         return result
 
-
     def setInicialToken(self, token):
-        loToken           = Token()
-        loToken.codigo    = token
-        loToken.pessoa    = self
+        loToken = Token()
+        loToken.codigo = token
+        loToken.pessoa = self
         loToken.save()
         return True
 
-
     def setInicialBalance(self, value):
-        loBalance           = Balance()
-        loBalance.valor     = value
-        loBalance.pessoa    = self
+        loBalance = Balance()
+        loBalance.valor = value
+        loBalance.pessoa = self
         loBalance.save()
         return True
 
@@ -81,23 +77,24 @@ class Token(models.Model):
 
     def __str__(self):
         return '%s : %s' % (
-            self.pessoa.nome, 
+            self.pessoa.nome,
             self.codigo
         )
 
+
 class Balance(models.Model):
-    valor       = models.DecimalField(null=True, max_digits=12, decimal_places=5, default=0)
-    pessoa      = models.ForeignKey("Pessoa", on_delete=models.CASCADE)
-    tx_token    = models.CharField(max_length=100, blank=True, default='')
-    cancelado   = models.BooleanField(default=False)
-    created_at  = models.DateTimeField(auto_now_add=True)
+    valor = models.DecimalField(
+        null=True, max_digits=12, decimal_places=5, default=0)
+    pessoa = models.ForeignKey("Pessoa", on_delete=models.CASCADE)
+    tx_token = models.CharField(max_length=100, blank=True, default='')
+    cancelado = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '%s | R$ %s' % (
-            self.pessoa.nome, 
+            self.pessoa.nome,
             self.valor
         )
-
 
 class History(models.Model):
 
@@ -113,31 +110,28 @@ class History(models.Model):
         (C, 'Cancelado')
     )
 
-    remetente       = models.ForeignKey("Pessoa", on_delete=models.CASCADE)
-    destino         = models.ForeignKey("Pessoa", related_name="Pessoa", on_delete=models.CASCADE)
-    token_destino   = models.CharField(max_length=100)
-    valor           = models.DecimalField(null=True, max_digits=12, decimal_places=5, default=0)
-    created_at      = models.DateField(auto_now_add=True)
-    update_at       = models.DateTimeField(auto_now=True)
-    status          = models.CharField(max_length=10, choices=STATUS_CHOICES, default=P)
-    auth_token      = models.CharField(max_length=100, blank=True)
+    remetente = models.ForeignKey("Pessoa", on_delete=models.CASCADE)
+    destino = models.ForeignKey(
+        "Pessoa", related_name="Pessoa", on_delete=models.CASCADE)
+    token_destino = models.CharField(max_length=100)
+    valor = models.DecimalField(
+        null=True, max_digits=12, decimal_places=5, default=0)
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=P)
+    auth_token = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return '%s  ->  R$ %s  ->  %s -> cpf %s | %s' % (
-            self.remetente.nome, 
+            self.remetente.nome,
             self.valor,
-            self.destino.nome, 
-            self.destino.cpf, 
+            self.destino.nome,
+            self.destino.cpf,
             self.status
-        )    
-
+        )
 
     def save(self, *args, **kwargs):
         # Gera o token da transacao
         if not self.auth_token:
-            self.auth_token = uuid.uuid4()            
+            self.auth_token = uuid.uuid4()
         super(History, self).save(*args, **kwargs)
-
-
-
-
--- pytransfer/views.py
+++ pytransfer/views.py
@@ -1,24 +1,19 @@
-from .models                        import History, Pessoa
-from .serializers                   import PessoaSerializer, HistorySerializer
-
-from rest_framework.views           import APIView
-from rest_framework.response        import Response
-from rest_framework                 import status, generics, filters
-
-from rest_framework.exceptions      import NotFound
-from django.core.exceptions         import ObjectDoesNotExist
-from django.shortcuts               import get_object_or_404
-from django.shortcuts               import redirect
+from .models import History, Pessoa
+from .serializers import PessoaSerializer, HistorySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class PessoaListCreate(generics.ListCreateAPIView):
-    queryset            = Pessoa.objects.all()
-    serializer_class    = PessoaSerializer
-    
+    queryset = Pessoa.objects.all()
+    serializer_class = PessoaSerializer
+
 
 class PessoaRetrive(generics.RetrieveUpdateDestroyAPIView):
-    queryset            = Pessoa.objects.all()
-    serializer_class    = PessoaSerializer
+    queryset = Pessoa.objects.all()
+    serializer_class = PessoaSerializer
 
     # desativa pessoa no banco
     def perform_destroy(self, instance):
@@ -27,24 +22,27 @@ class PessoaRetrive(generics.RetrieveUpdateDestroyAPIView):
 
 
 class HistorysAPIView(generics.ListAPIView):
-    serializer_class    = HistorySerializer
+    serializer_class = HistorySerializer
 
     def get_queryset(self):
-        queryset    = History.objects.all()
-        parametros  = self.request.query_params # busca a lista de argumentos GET
+        queryset = History.objects.all()
+        parametros = self.request.query_params  # busca a lista de argumentos GET
 
         # filtra todo historico por pessoa "Remetente"
-        if self.kwargs.get('pessoa_pk'):            
-            queryset = self.queryset.filter(remetente_id=self.kwargs.get('pessoa_pk'))
+        if self.kwargs.get('pessoa_pk'):
+            queryset = self.queryset.filter(
+                remetente_id=self.kwargs.get('pessoa_pk'))
 
         # filtra entre datas passadas como parametros na url.
         if parametros.get('start', None) is not None and parametros.get('end', None) is not None:
-            queryset = queryset.filter(created_at__range=(parametros.get('start'), parametros.get('end')))
+            queryset = queryset.filter(created_at__range=(
+                parametros.get('start'), parametros.get('end')))
 
         # filtra por cpf do destinatario nos registros de transaçoes.
         if parametros.get('cpf'):
             try:
-                loDestino = Pessoa.objects.filter(cpf=parametros.get('cpf')).last()
+                loDestino = Pessoa.objects.filter(
+                    cpf=parametros.get('cpf')).last()
                 if loDestino is not None:
                     queryset = queryset.filter(destino_id=loDestino.pk)
             except ObjectDoesNotExist:
@@ -66,7 +64,7 @@ class HistoryListAndCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class HistoryListCreate(generics.ListCreateAPIView):
-    queryset            = History.objects.all()
-    serializer_class    = HistorySerializer
-    
+    queryset = History.objects.all()
+    serializer_class = HistorySerializer
